b612fae18449ed28f1ab68ca734211b853c30e1c
rahad2694 <rahad.haw@gmail.com>
2022-06-06 04:47:51 +0600

    Make Appointment section added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from './Banner';
 import Banner_2 from './Banner_2';
+import Banner_3_appoinment from './Banner_3_appoinment';
 import Services from './Services';
 
 
@@ -14,6 +15,7 @@ const Home = () => {
             </div>
             <Services></Services>
             <Banner_2></Banner_2>
+            <Banner_3_appoinment></Banner_3_appoinment>
 
         </div>
     );
