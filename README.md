c095d10ab23449fb25da4e5398627ae961a1ba83
rahad2694 <rahad.haw@gmail.com>
2022-06-06 06:14:04 +0600

    Landing page finalized

--- README.md
+++ README.md
@@ -2,17 +2,17 @@
 
 ### An "Appointment Booking" Site's Landing Page
 
-For Live Site Link [Click Here](https://tools-manufacturer-a873a.web.app/).
+For Live Site Link [Click Here](https://doctors-booking-page.netlify.app/).
 
 ## Project Features:
 
-- Landing page is responsive in Desktop, Tablet and in Mobile version.
-- Hamburger Menu is added in mobile version.
+- Landing page is responsive for Desktop, Tablet and in Mobile version.
+- Hamburger Menu is added for mobile version.
 - 404 Not Found route is added.
-- React Router used to navigate to routes.
+- React Router is used to navigate routes.
 - Navigable Carousel added.
 
-### Frameworks / Libraries used in Front-End:
+### Frameworks / Libraries used:
 
 - Tailwind
 - Daisy UI
--- src/pages/shared/NavBar.js
+++ src/pages/shared/NavBar.js
@@ -12,9 +12,9 @@ const NavBar = () => {
         <li><Link to="/login">Login</Link></li>
     </>
     return (
-        <div className="navbar bg-base-100 sticky z-50 top-0 lg:px-10 md:px-4 px-2">
+        <div className="navbar bg-base-100 sticky z-50 top-0 lg:px-10 ">
             <div className="navbar-start">
-                <div className="dropdown">
+                <div className="dropdown ">
                     <label tabIndex="0" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
